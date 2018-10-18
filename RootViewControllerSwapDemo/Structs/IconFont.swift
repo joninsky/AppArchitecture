3349b5f13c4b2f528106f22c4d63b665b3b11aa4
Jon Vogel <jonvogel@Jons-MacBook-Pro-2.local>
2018-10-17 19:50:49 -0700

    Added Fonts

---
+++ RootViewControllerSwapDemo/Structs/IconFont.swift
@@ -0,0 +1,14 @@
+//
+//  IconFont.swift
+//  RootViewControllerSwapDemo
+//
+//  Created by Jon Vogel on 10/17/18.
+//  Copyright © 2018 Nomad Company. All rights reserved.
+//
+
+import Foundation
+
+
+struct IconFont {
+    
+}
